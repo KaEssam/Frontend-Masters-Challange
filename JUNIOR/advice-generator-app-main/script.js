792726aadb57bca1872a8cd26b9557750e40b040
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2024-07-21 18:39:31 +0300

    advice generator app

---
+++ JUNIOR/advice-generator-app-main/script.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  const getAdvice = async () => {
+    const id = Math.floor(Math.random() * 223) + 1;
+    const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+    const advice = await response.json();
+    return advice;
+  };
+
+  const setAdvice = () => {
+    getAdvice()
+      .then(response => {
+        const {
+          slip: { id, advice },
+        } = response;
+        document.getElementById("AdviceTitle").innerHTML = `Advice #${id}`;
+        document.getElementById("AdviceText").innerHTML = `"${advice}"`;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  document.getElementById("Dice").addEventListener("click", setAdvice);
+};
